0cdfb7fd373ea8a2e72e3d949c792c82af2067d0
TomekBekus02 <156850117+TomekBekus02@users.noreply.github.com>
2025-08-07 14:12:55 +0200

    create main page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Slack } from 'lucide-react';
+import classes from './dashboard.module.css'
 import "./globals.css";
+import Link from "next/link";
 
 export default function RootLayout({
   children,
@@ -10,6 +13,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <nav className={classes.navBox}>
+          <div className={classes.logoBox}>
+            <Link href="*"><Slack /> Football Academy</Link>
+          </div>
+          <ul className={classes.linksBox}>
+            <div className={classes.tabContainer}>
+              <li><Link href="*">Zawodnicy</Link></li>
+              <li><Link href="*">Sztab</Link></li>
+              <li><Link href="*">Kontakt</Link></li>
+              <li><Link href="*">Kontakt</Link></li>
+            </div>
+            <div className={classes.joinUsButtonContainer}>
+              <li><Link href="*">Dołącz do nas</Link></li>
+            </div>
+          </ul>
+        </nav>
         {children}
       </body>
     </html>
